1db2a6057fed3705701e49ac8c5e70ec919f48ed
hoangnhimgia <giangthaihoangmacair@gmail.com>
2025-07-08 08:48:57 +0700

    Update server.py

--- server.py
+++ server.py
@@ -11,21 +11,22 @@ def home():
 
 @app.route("/send-email", methods=["POST"])
 def send_email():
-    data = request.json
-    receiver = data.get("to")
-    subject = data.get("subject")
-    html_content = data.get("htmlBody")
-
-    msg = MIMEText(html_content, "html")
-    msg["Subject"] = subject
-    msg["From"] = "thgiang.hcm@vnpt.vn"
-    msg["To"] = receiver
-
     try:
+        data = request.json
+        receiver = data.get("to")
+        subject = data.get("subject")
+        html_content = data.get("htmlBody")
+
+        msg = MIMEText(html_content, "html")
+        msg["Subject"] = subject
+        msg["From"] = "thgiang.hcm@vnpt.vn"
+        msg["To"] = receiver
+
         with smtplib.SMTP("email.vnpt.vn", 587) as server:
             server.starttls()
             server.login("thgiang.hcm@vnpt.vn", os.environ.get("EMAIL_PASSWORD"))
             server.sendmail("thgiang.hcm@vnpt.vn", receiver, msg.as_string())
+
         return {"status": "success"}, 200
     except Exception as e:
         return {"status": "error", "message": str(e)}, 500
