06be4f71400e38e6d817d5c299c655d3ba397da3
hoangnhimgia <giangthaihoangmacair@gmail.com>
2025-07-08 08:24:01 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Server Flask của Giang đã lên sóng!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
