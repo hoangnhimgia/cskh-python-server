f9c859b46182b01a663fc823af6283d0d1dba753
hoangnhimgia <giangthaihoangmacair@gmail.com>
2025-07-15 08:04:23 +0700

    server.py

--- server.py
+++ server.py
@@ -26,7 +26,8 @@ def send_email():
 
         msg = MIMEText(html_content, "html")
         msg["Subject"] = subject
-        msg["From"] = "VNPT THU DUC-CSKH <thgiang.hcm@vnpt.vn>"
+        from_name = data.get("from_name", "VNPT TPHCM - CSKH")
+        msg["From"] = f"{from_name} <thgiang.hcm@vnpt.vn>"
         msg["To"] = receiver
 
         with smtplib.SMTP("email.vnpt.vn", 587) as server:
